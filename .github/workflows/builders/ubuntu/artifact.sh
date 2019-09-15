cd836aa642677ea73ebb8e0e2343a3e3b0be2815
chetabahana <chetabahana@gmail.com>
2019-09-15 02:08:00 +0000

    Add Support for CI/CD Workflows

--- .github/workflows/builders/ubuntu/artifact.sh
+++ .github/workflows/builders/ubuntu/artifact.sh
@@ -13,28 +13,24 @@ gcloud info
 python --version
 
 echo -e "\n$hr\nHOME PROFILES\n$hr"
-echo $HOME
 ls -al $HOME
 
 echo -e "$hr\nROOT PROFILES\n$hr"
-echo "/root -> $HOME"
-[[ "$(whoami)" == "root" ]] && ls -alL /root || sudo bash -c 'ls -al /root'
+[[ "$(whoami)" == "root" ]] && ls -alL /root || sudo bash -c 'ls -alL /root'
 
 echo -e "$hr\nSSH FILES\n$hr"
-echo "/root/.ssh -> $HOME/.ssh"
-[[ "$(whoami)" == "root" ]] && ls -alL /root/.ssh || sudo bash -c 'ls -al /root/.ssh'
+echo $(whoami) /root/.ssh
+[[ "$(whoami)" == "root" ]] && ls -alL /root/.ssh || sudo bash -c 'ls -alL /root/.ssh'
 
-echo -e "\n$hr\nENVIRONTMENT\n$hr"
-HR=$hr && unset hr
-HRD=$hrd && unset hrd
-printenv | sort
-export hr=$HR
-export hrd=$HRD
+echo -e "$hr\nBIN FILES\n$hr"
+echo $HOME/.local/bin
+ls -al $HOME/.local/bin
 
 echo -e "\n$hr\nFILE SYSTEM\n$hr"
 df -h
 
 echo -e "\n$hr\nALL REPOSITORY\n$hr"
+pwd
 ls -al /
 
 echo -e "\n$hr\nCURRENT REPOSITORY\n$hr"
--- .google/cloud/builders/gcloud/artifact.sh
+++ .google/cloud/builders/gcloud/artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "$hr\nWHOAMI\n$hr"
+echo -e "WHOAMI\n$hr"
 whoami
 echo $HOME
 id
@@ -13,10 +13,16 @@ gcloud info
 python --version
 
 echo -e "\n$hr\nHOME PROFILES\n$hr"
+echo $HOME
 ls -al $HOME
 
+echo -e "$hr\nROOT PROFILES\n$hr"
+echo "/root -> $HOME"
+[[ "$(whoami)" == "root" ]] && ls -alL /root || sudo bash -c 'ls -al /root'
+
 echo -e "$hr\nSSH FILES\n$hr"
-ls -lL /root/.ssh
+echo "/root/.ssh -> $HOME/.ssh"
+[[ "$(whoami)" == "root" ]] && ls -alL /root/.ssh || sudo bash -c 'ls -al /root/.ssh'
 
 echo -e "\n$hr\nENVIRONTMENT\n$hr"
 HR=$hr && unset hr
@@ -29,9 +35,21 @@ echo -e "\n$hr\nFILE SYSTEM\n$hr"
 df -h
 
 echo -e "\n$hr\nALL REPOSITORY\n$hr"
-pwd
 ls -al /
 
 echo -e "\n$hr\nCURRENT REPOSITORY\n$hr"
 pwd
 ls -al .
+
+[[ ! -x "$(command -v docker)" ]] && exit 0
+echo -e "\n$hr\nDOCKER VERSION\n$hr"
+docker version
+
+echo -e "\n$hr\nDOCKER INFO\n$hr"
+docker info
+
+echo -e "$hr\nIMAGE BUILDERS\n$hr"
+docker images --all | sort
+
+echo -e "\n$hr\nCURRENTLY RUNNING\n$hr"
+docker ps
